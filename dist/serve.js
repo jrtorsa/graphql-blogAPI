59554bf90bb520e5deed40094d2ff902aefbe593
Raul T <jr.torsa@gmail.com>
2020-09-23 14:02:16 -0600

    wrapping up

--- dist/serve.js
+++ dist/serve.js
@@ -6,8 +6,6 @@ var _express2 = _interopRequireDefault(_express);
 
 var _expressGraphql = require("express-graphql");
 
-var _expressGraphql2 = _interopRequireDefault(_expressGraphql);
-
 var _schema = require("./schema");
 
 var _schema2 = _interopRequireDefault(_schema);
@@ -16,10 +14,10 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 var app = (0, _express2.default)();
 
-app.use("/graphql", (0, _expressGraphql2.default)({
+app.use("/graphql", (0, _expressGraphql.graphqlHTTP)({
   schema: _schema2.default,
   graphiql: true
 }));
 
-console.log("Starting the server...");
 app.listen(4000);
+console.log("Starting the server...");
