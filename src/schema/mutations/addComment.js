08eb15b077989ae8b828702d732486d85e952170
Raul T <jr.torsa@gmail.com>
2020-09-23 12:42:36 -0600

    implementing mutations

---
+++ src/schema/mutations/addComment.js
@@ -0,0 +1,16 @@
+import { GraphQLNonNull, GraphQLInt } from "graphql";
+import { fakeDatabase } from "../../FakeDatabase";
+import { Comment, CommentInputType } from "../types/Comment";
+
+export default {
+  addComment: {
+    type: Comment,
+    description: "Creates a new comment for a blog post",
+    args: {
+      post: { type: CommentInputType },
+    },
+    resolve: function (parent, { comment }) {
+      return fakeDatabase.addNewComment(comment);
+    },
+  },
+};
---
+++ src/schema/mutations/addPost.js
@@ -0,0 +1,16 @@
+import { GraphQLNonNull, GraphQLInt } from "graphql";
+import { fakeDatabase } from "../../FakeDatabase";
+import { Post, PostInputType } from "../types/Post";
+
+export default {
+  addPost: {
+    type: Post,
+    description: "Creates a new blog post",
+    args: {
+      post: { type: PostInputType },
+    },
+    resolve: function (parent, { post }) {
+      return fakeDatabase.addNewBlogPost(post);
+    },
+  },
+};
